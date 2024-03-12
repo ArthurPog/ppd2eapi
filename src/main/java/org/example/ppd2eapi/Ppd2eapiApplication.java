b93ea639e89e9a93271505a0f4917f16acd1837a
Arthur Poghosyan <arthurpoghosyan@gmail.com>
2024-03-12 17:15:02 +0100

    Dev environment initialization

---
+++ src/main/java/org/example/ppd2eapi/Ppd2eapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ppd2eapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppd2eapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppd2eapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ppd2eapi/Ppd2eapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ppd2eapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppd2eapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
