9f7b46063b0e4e869f695eeebc082fffff1e3e7f
Shimmyland <simonlibiger@gmail.com>
2024-03-13 14:22:38 +0100

    idk

--- src/main/java/org/example/ppd2eapi/Ppd2eapiApplication.java
+++ src/main/java/org/example/ppd2eapi/Ppd2eapiApplication.java
@@ -1,21 +1,10 @@
 package org.example.ppd2eapi;
 
-import org.example.ppd2eapi.services.MovieService;
-import org.example.ppd2eapi.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ppd2eapiApplication {
-
-    // dependencies
-    private final UserService userService;
-    private final MovieService movieService;
-
-    public Ppd2eapiApplication(UserService userService, MovieService movieService) {
-        this.userService = userService;
-        this.movieService = movieService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Ppd2eapiApplication.class, args);
