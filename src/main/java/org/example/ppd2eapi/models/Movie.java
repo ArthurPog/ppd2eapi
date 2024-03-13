5f101ddc2ae9c95e7de6e76449b16313d3504f91
Filip Novakov <filip.novakov13@gmail.com>
2024-03-13 11:14:06 +0100

    created models for user and movie

---
+++ src/main/java/org/example/ppd2eapi/models/Movie.java
@@ -0,0 +1,37 @@
+package org.example.ppd2eapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    public Movie() {
+    }
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
