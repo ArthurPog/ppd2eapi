58cace32dee851e6869957b9667f89601219d418
Shimmyland <simonlibiger@gmail.com>
2024-03-14 11:11:24 +0100

    idk

--- src/main/java/org/example/ppd2eapi/services/implications/MovieServiceImp.java
+++ src/main/java/org/example/ppd2eapi/services/implications/MovieServiceImp.java
@@ -2,7 +2,9 @@ package org.example.ppd2eapi.services.implications;
 
 import org.example.ppd2eapi.repositories.MovieRepo;
 import org.example.ppd2eapi.services.MovieService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MovieServiceImp implements MovieService {
 
     // dependencies
